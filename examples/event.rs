13b9f4aa17a09dffb9c022094f886396f9363c69
Joel Nordström <e.joel.nordstrom@gmail.com>
2020-10-14 21:30:05 +0200

    Update example to use new glutin

--- examples/event.rs
+++ examples/event.rs
@@ -6,27 +6,40 @@ use scancode::Scancode;
 
 fn main() {
     let mut errors = BTreeMap::new();
-    let events_loop = glutin::EventsLoop::new();
-    let window = glutin::WindowBuilder::new().build(&events_loop).unwrap();
-    let _ = unsafe { window.make_current() };
+    let events_loop = glutin::event_loop::EventLoop::new();
+    let window_builder = glutin::window::WindowBuilder::new();
+    let context = glutin::ContextBuilder::new()
+        .build_windowed(window_builder, &events_loop).unwrap();
+    let context = unsafe { context.make_current().unwrap() };
     // println!("Pixel format of the window: {:?}", window.get_pixel_format());
     // let context = support::load(&window);
 
-    events_loop.run_forever(|event| {
-        let _ = window.swap_buffers();
+    events_loop.run(move |event, _, control_flow| {
+        let _ = context.swap_buffers();
 
-        let glutin::Event::WindowEvent { event, .. } = event;
+        let event = match event {
+            glutin::event::Event::WindowEvent { event, .. } => event,
+            _ => return,
+        };
 
         match event {
-            glutin::WindowEvent::KeyboardInput(state, scancode, vk, _) => {
-                if state == glutin::ElementState::Pressed {
+            glutin::event::WindowEvent::KeyboardInput {
+                input: glutin::event::KeyboardInput {
+                    state,
+                    scancode,
+                    virtual_keycode: vk,
+                    ..
+                },
+                ..
+            }=> {
+                if state == glutin::event::ElementState::Pressed {
                     print!("pressed")
                 } else {
                     print!("released")
                 }
 
                 print!(" {} -> ", scancode);
-                if let Some(code) = Scancode::new(scancode) {
+                if let Some(code) = Scancode::new(scancode as u8) {
                     print!("{:?}", code);
                 } else {
                     print!("*** ERROR: UNKNOWN SCANCODE ***");
@@ -39,15 +52,17 @@ fn main() {
                     println!(" (virtual keycode UNKNOWN)");
                 }
             }
-            glutin::WindowEvent::Closed => events_loop.interrupt(),
+            glutin::event::WindowEvent::CloseRequested => {
+                *control_flow = glutin::event_loop::ControlFlow::Exit;
+
+                if !errors.is_empty() {
+                    println!("Unhandled scancodes:");
+                    for (k, v) in errors.iter() {
+                        println!("{}: {}", k, v);
+                    }
+                }
+            }
             _ => (),
         }
     });
-
-    if !errors.is_empty() {
-        println!("Unhandled scancodes:");
-        for (k, v) in errors.iter() {
-            println!("{}: {}", k, v);
-        }
-    }
 }
